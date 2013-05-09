d8dbf853927e1d78d7aa489258fe96889b949de5
Anton Stöckl <anton@stoeckl.de>
2013-05-09 15:07:37 +0200

    fixed chdir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
  * This makes our life easier when dealing with paths.
  * Everything is relative to the application root now.
  */
-dirname(dirname(dirname(__DIR__)));
+chdir(dirname(__DIR__));
 
 // Setup autoloading
 $loader = require_once 'tests/autoload.php';
